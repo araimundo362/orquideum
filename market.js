bcfc9c5ad66365d930da0ca4e06be0bdfba361b3
Tony362 <araimundo362@gmail.com>
2021-02-07 13:33:03 -0300

    fix(*0): fixed some bugs and clean unnecesary code

--- market.js
+++ market.js
@@ -1,4 +1,4 @@
-// Desafio 9 - Eventos.
+// 2da entrega Proyecto Final -- Javascript.
 
 /********************************************************************************************************************* */
 // Clase Orquidea: genero, especie, precio, id e imagen. 
@@ -22,8 +22,8 @@ class Orchid {
     this.stock = this.stock - cant;
   }
 
-  plusStock(cant) {
-    this.stock = this.stock + cant;
+  plusStock() {
+    this.stock = this.stock + 1;
   }                 
 }
 
@@ -45,7 +45,8 @@ class Carrito {
         this.contentList = this.contentList.concat([orq]);
       }
       this.total = this.total + cant * orq.precio;
-      orq.minusStock(cant);  
+      orq.minusStock(cant); 
+      localStorage.setItem('carrito', carrito); 
     } else {
       let alertError = `
         <div class="alert alert-danger" role="alert" style="margin-top: 15px">
@@ -63,11 +64,17 @@ class Carrito {
   removeFromCarrito(orq) {
     let idList = this.contentList.map((elem) => elem.id);
     console.log('idList', idList)
-    if(idList.includes(orq.id)) { // [1,1,1,1, 2, 5] ???
-      let prevLength = this.contentList.length;
-      this.contentList = this.contentList.filter( (orchid) => orchid.id !== orq.id);
-      this.total = this.total - (prevLength - this.contentList.length) * orq.precio;
-      orq.plusStock(prevLength - this.contentList.length);
+    if(idList.includes(orq.id)) {
+      let index = this.contentList.findIndex((orch) => orch.id === orq.id);
+      let contentAux= [];
+      for(let k=0; k<this.contentList.length; k++) {
+        if(k!==index) {
+          contentAux.push(this.contentList[k])
+        }
+      }
+      this.contentList = contentAux;
+      this.total = this.total - orq.precio;
+      orq.plusStock();
     } else {
       let alertError = `
         <div class="alert alert-danger" role="alert" style="margin-top: 15px">
@@ -94,7 +101,7 @@ let orquideum = [
   new Orchid( 2 , 'Dendrobium', 'Nobile', 1849, 2, "imagenes/dendrobium-nobile.jpg"),
   new Orchid( 3 ,'Laelia', 'Tenebrosa', 2000, 3, "imagenes/Laelia-tenebrosa.jpg"),
   new Orchid( 4, 'Cymbidium', 'Briggitte-Bardot', 3599, 1, "imagenes/cymbidium-BriggitteBardot.jpg" ),
-  new Orchid( 5, 'Cattleya', 'Little-Toshie', 720, 0, "imagenes/cat-LittleToshie.jpg" ),
+  new Orchid( 5, 'Cattleya', 'Little-Toshie', 720, 10, "imagenes/cat-LittleToshie.jpg" ),
   new Orchid( 6, 'Cattleya', 'Scholfeldana', 1299, 1,"imagenes/cat-scholfeldana.jpg" ),
   new Orchid( 7, 'Cattleya', 'Sophroniti-Cernua', 4999, 6 ,"imagenes/cat-sophronitiCernua.jpg"),
   new Orchid( 8, 'Odontocidium', 'Susan-Keller', 1800, 7, "imagenes/odonto-SusanKeller.jpg" )
@@ -125,24 +132,6 @@ for (let i = 0; i < orquideum.length; i++) {
           </div>
         </div>
         `;
-  }else {
-    item +=
-      ` <div class="col-lg-4 col-md-6 mb-4">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top market__img" src=${orquideum[i].imagen} alt="imagen flor"></a>
-          <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">${orquideum[i].genero} ${orquideum[i].especie}</a>
-          </h4>
-          <h5>$${orquideum[i].precio}</h5>
-          <input placeholder="Cantidad" value="" class="form-control form-control-rounded" type="number" id="input${orquideum[i].id}">
-        </div>
-        <div class="card-footer">
-          <button type="button" class="btn btn-primary" onclick="carrito.addToCarrito(orquideum[${i}])">Comprar</button>
-          <button type="button" class="btn btn-primary" onclick="carrito.removeFromCarrito(orquideum[${i}])">Sacar del carrito</button>
-        </div>
-      </div>
-    </div>`
   }
 }
 
@@ -199,7 +188,6 @@ function checkForOrchid(orq, ar) {
       return true;
     }
   }
-
   return false;
 } 
 
@@ -224,9 +212,9 @@ function filterByPrice(desde, hasta) {
   setFilterState(filterList, priceFilteredList);
 }
 
-function setFilterState(filterArray, priceFilterArray) {
-  let newFilterState = [];
+let newFilterState = [];
 
+function setFilterState(filterArray, priceFilterArray) {
   // Hago una interseccion entre los arreglos en caso de tener distintos filtros. 
   // Si alguno es vacio, "devuelvo" el otro. 
   // Si los 2 son vacios, volvemos al estado original
@@ -235,7 +223,7 @@ function setFilterState(filterArray, priceFilterArray) {
       if(priceFilterArray.includes(filterArray[i])) {
         newFilterState.push(filterArray[i]);
       }
-    }
+    }     
   } else {
     if (!filterArray.length) {
       newFilterState = priceFilterArray;
@@ -269,26 +257,10 @@ function setFilterState(filterArray, priceFilterArray) {
               </div>
             </div>
             `;
-      }else {
-        filterItems +=
-          ` <div class="col-lg-4 col-md-6 mb-4">
-              <div class="card h-100">
-                <a href="#"><img class="card-img-top market__img" src=${newFilterState[i].imagen} alt="imagen flor"></a>
-              <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">${newFilterState[i].genero} ${newFilterState[i].especie}</a>
-              </h4>
-              <h5>$${newFilterState[i].precio}</h5>
-              <input placeholder="Cantidad" value="" class="form-control form-control-rounded" type="number" id="input${newFilterState[i].id}">
-            </div>
-            <div class="card-footer">
-              <button type="button" class="btn btn-primary" onclick="carrito.addToCarrito(newFilterState[${i}])">Comprar</button>
-              <button type="button" class="btn btn-primary" onclick="carrito.removeFromCarrito(newFilterState[${i}])">Sacar del carrito</button>
-            </div>
-          </div>
-        </div>`
       }
     }
+
+    console.log('veo el filterState', newFilterState)
     //Borro el elemento menu del DOM.
     document.getElementById('menu').remove();
     //Creo el contenido con el nuevo listado.
